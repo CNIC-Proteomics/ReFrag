7c5e38dd5f22a54aca124f2706f2414ed0853056
Andrea <andrea.laguillo@externo.cnic.es>
2022-06-28 13:47:58 +0200

    get matched ions and proof

--- ReFrag.py
+++ ReFrag.py
@@ -13,11 +13,14 @@ import configparser
 import itertools
 import logging
 import math
+import numpy as np
 import os
 import pandas as pd
 from pathlib import Path
 import pyopenms
 import re
+import scipy.stats
+import statistics
 import sys
 from tqdm import tqdm
 
@@ -112,20 +115,282 @@ def insertMods(peptide, mods):
             pos = len(peptide)
         modlist.append([pos, value])
     modlist = modlist[::-1] # Reverse list so it can be added without breaking pos
+    omod = []
+    opos = []
     for pos, value in modlist:
-        peptide = peptide[:pos] + '[' + str(value) + ']' + peptide[pos:] 
+        peptide = peptide[:pos] + '[' + str(value) + ']' + peptide[pos:]
+        omod.append(value)
+        opos.append(pos)
     return(peptide)
 
-def parallelFragging(query):
+def getTheoMH(charge, sequence, mods, pos, nt, ct):
+    '''    
+    Calculate theoretical MH using the PSM sequence.
+    '''
+    AAs = dict(mass._sections['Aminoacids'])
+    MODs = dict(mass._sections['Fixed Modifications'])
+    m_proton = mass.getfloat('Masses', 'm_proton')
+    m_hydrogen = mass.getfloat('Masses', 'm_hydrogen')
+    m_oxygen = mass.getfloat('Masses', 'm_oxygen')
+    total_aas = 2*m_hydrogen + m_oxygen
+    total_aas += charge*m_proton
+    #total_aas += float(MODs['nt']) + float(MODs['ct'])
+    if nt:
+        total_aas += float(MODs['nt'])
+    if ct:
+        total_aas += float(MODs['ct'])
+    for i, aa in enumerate(sequence):
+        if aa.lower() in AAs:
+            total_aas += float(AAs[aa.lower()])
+        if aa.lower() in MODs:
+            total_aas += float(MODs[aa.lower()])
+        # if aa.islower():
+        #     total_aas += float(MODs['isolab'])
+        if i in pos:
+            total_aas += float(mods[pos.index(i)])
+    MH = total_aas - (charge-1)*m_proton
+    return MH
+
+def expSpectrum(fr_ns, scan, index2, mode):
+    '''
+    Prepare experimental spectrum.
+    '''
+    # index1 = fr_ns.loc[fr_ns[0]=='SCANS='+str(scan)].index[0] + 1
+    # index2 = fr_ns.drop(index=fr_ns.index[:index1], axis=0).loc[fr_ns[0]=='END IONS'].index[0]
+    
+    if mode == "mgf":
+        index1 = fr_ns.to_numpy() == 'SCANS='+str(int(scan))
+        index1 = np.where(index1)[0][0]
+        index3 = np.where(index2)[0]
+        index3 = index3[np.searchsorted(index3,[index1,],side='right')[0]]
+        
+        try:
+            ions = fr_ns.iloc[index1+1:index3,:]
+            ions[0] = ions[0].str.strip()
+            ions[['MZ','INT']] = ions[0].str.split(" ",expand=True,)
+            ions = ions.drop(ions.columns[0], axis=1)
+            ions = ions.apply(pd.to_numeric)
+        except ValueError:
+            ions = fr_ns.iloc[index1+4:index3,:]
+            ions[0] = ions[0].str.strip()
+            ions[['MZ','INT']] = ions[0].str.split(" ",expand=True,)
+            ions = ions.drop(ions.columns[0], axis=1)
+            ions = ions.apply(pd.to_numeric)
+    elif mode == "mzml":
+        for s in fr_ns.getSpectra():
+            # Keep only scans in range
+            if int(s.getNativeID().split(' ')[-1][5:]) == int(scan):
+                ions = pd.DataFrame([s.get_peaks()[0], s.get_peaks()[1]]).T
+                ions.columns = ["MZ", "INT"]
+                break # Scan numbers are unique
+        
+    ions["ZERO"] = 0
+    #ions["CCU"] = 0.01
+    ions["CCU"] = ions.MZ - 0.01
+    ions.reset_index(drop=True)
+    
+    #bind = pd.DataFrame(list(itertools.chain(*set(zip(list(ions['CCU']),list(ions['MZ']))))), columns=["MZ"])
+    #bind["REL_INT"] = list(itertools.chain(*set(zip(list(ions['ZERO']),list(ions['INT'])))))
+    bind = pd.DataFrame(list(itertools.chain.from_iterable(zip(list(ions['CCU']),list(ions['MZ'])))), columns=["MZ"])
+    bind["REL_INT"] = list(itertools.chain.from_iterable(zip(list(ions['ZERO']),list(ions['INT']))))
+    bind["ZERO"] = 0
+    bind["CCU"] = bind.MZ + 0.01
+    
+    spec = pd.DataFrame(list(itertools.chain.from_iterable(zip(list(bind['MZ']),list(bind['CCU'])))), columns=["MZ"])
+    spec["REL_INT"] = list(itertools.chain.from_iterable(zip(list(bind['REL_INT']),list(bind['ZERO']))))
+    
+    median_rel_int = statistics.median(ions.INT)
+    std_rel_int = np.std(ions.INT, ddof = 1)
+    ions["NORM_REL_INT"] = (ions.INT - median_rel_int) / std_rel_int
+    ions["P_REL_INT"] = scipy.stats.norm.cdf(ions.NORM_REL_INT) #, 0, 1)
+    normspec = ions.loc[ions.P_REL_INT>0.81]
+    if len(ions) > 0 and len(normspec) > 0:
+        spec_correction = max(ions.INT)/statistics.mean(normspec.INT)
+    else: spec_correction = 0
+    spec["CORR_INT"] = spec.REL_INT*spec_correction
+    spec.loc[spec['CORR_INT'].idxmax()]['CORR_INT'] = max(spec.REL_INT)
+    spec["CORR_INT"] = spec.apply(lambda x: max(ions.INT)-13 if x["CORR_INT"]>max(ions.INT) else x["CORR_INT"], axis=1)
+    return(spec, ions, spec_correction)
+
+def theoSpectrum(seq, mods, pos, len_ions, dm):
+    '''
+    Prepare theoretical fragment matrix.
+
+    '''
+    m_hydrogen = mass.getfloat('Masses', 'm_hydrogen')
+    m_oxygen = mass.getfloat('Masses', 'm_oxygen')
+    ## Y SERIES ##
+    #ipar = list(range(1,len(seq)))
+    outy = pd.DataFrame(np.nan, index=list(range(1,len(seq)+1)), columns=list(range(1,len_ions+1)))
+    for i in range(0,len(seq)):
+        yn = list(seq[i:])
+        if i > 0: nt = False
+        else: nt = True
+        fragy = getTheoMH(0,yn,mods,pos,nt,True) + dm
+        outy[i:] = fragy
+        
+    ## B SERIES ##
+    outb = pd.DataFrame(np.nan, index=list(range(1,len(seq)+1)), columns=list(range(1,len_ions+1)))
+    for i in range(0,len(seq)):
+        bn = list(seq[::-1][i:])
+        if i > 0: ct = False
+        else: ct = True
+        fragb = getTheoMH(0,bn,mods,pos,True,ct) - 2*m_hydrogen - m_oxygen + dm
+        outb[i:] = fragb
+    
+    ## FRAGMENT MATRIX ##
+    yions = outy.T
+    bions = outb.iloc[::-1].T
+    spec = pd.concat([bions, yions], axis=1)
+    spec.columns = range(spec.columns.size)
+    spec.reset_index(inplace=True, drop=True)
+    return(spec)
+
+def errorMatrix(mz, theo_spec):
+    '''
+    Prepare ppm-error and experimental mass matrices.
+    '''
+    m_proton = mass.getfloat('Masses', 'm_proton')
+    exp = pd.DataFrame(np.tile(pd.DataFrame(mz), (1, len(theo_spec.columns)))) 
+    
+    ## EXPERIMENTAL MASSES FOR CHARGE 2 ##
+    mzs2 = pd.DataFrame(mz)*2 - m_proton
+    mzs2 = pd.DataFrame(np.tile(pd.DataFrame(mzs2), (1, len(exp.columns)))) 
+    
+    ## EXPERIMENTAL MASSES FOR CHARGE 3 ##
+    mzs3 = pd.DataFrame(mz)*3 - m_proton*2
+    mzs3 = pd.DataFrame(np.tile(pd.DataFrame(mzs3), (1, len(exp.columns)))) 
+    
+    ## PPM ERRORS ##
+    terrors = (((exp - theo_spec)/theo_spec)*1000000).abs()
+    terrors2 =(((mzs2 - theo_spec)/theo_spec)*1000000).abs()
+    terrors3 = (((mzs3 - theo_spec)/theo_spec)*1000000).abs()
+    return(terrors, terrors2, terrors3, exp)
+
+def makeFrags(seq_len):
+    '''
+    Name all fragments.
+    '''
+    frags = pd.DataFrame(np.nan, index=list(range(0,seq_len*2)),
+                         columns=["by", "by2", "by3", "bydm", "bydm2", "bydm3"])
+    frags.by = ["b" + str(i) for i in list(range(1,seq_len+1))] + ["y" + str(i) for i in list(range(1,seq_len+1))[::-1]]
+    frags.by2 = frags.by + "++"
+    frags.by3 = frags.by + "+++"
+    frags.bydm = frags.by + "*"
+    frags.bydm2 = frags.by + "*++"
+    frags.bydm3 = frags.by + "*+++"
+    return(frags)
+
+def assignIons(theo_spec, dm_theo_spec, frags, dm, arg_dm):
+    m_proton = mass.getfloat('Masses', 'm_proton')
+    assign = pd.concat([frags.by, theo_spec.iloc[0]], axis=1)
+    assign.columns = ['FRAGS', '+']
+    assign["++"] = (theo_spec.iloc[0]+m_proton)/2
+    assign["+++"] = (theo_spec.iloc[0]+2*m_proton)/3
+    assign["*"] = dm_theo_spec.iloc[0]
+    assign["*++"] = (dm_theo_spec.iloc[0]+m_proton)/2
+    c_assign = pd.DataFrame(list(assign["+"]) + list(assign["++"]) + list(assign["+++"]))
+    if dm >= arg_dm:
+        c_assign = pd.concat([c_assign, pd.DataFrame(list(assign["*"])), pd.DataFrame(list(assign["*++"]))])
+    c_assign.columns = ["MZ"]
+    c_assign_frags = pd.DataFrame(list(frags.by) + list(frags.by + "++") + list(frags.by + "+++"))
+    if dm >= arg_dm:
+        c_assign_frags = pd.concat([c_assign_frags, pd.DataFrame(list(frags.by + "*")), pd.DataFrame(list(frags.by + "*++"))])
+    c_assign["FRAGS"] = c_assign_frags
+    c_assign["ION"] = c_assign.apply(lambda x: re.findall(r'\d+', x.FRAGS)[0], axis=1)
+    c_assign["CHARGE"] = c_assign.apply(lambda x: x.FRAGS.count('+'), axis=1).replace(0, 1)
+    return(c_assign)
+
+def makeAblines(texp, minv, assign, ions):
+    masses = pd.concat([texp[0], minv], axis = 1)
+    matches = masses[(masses < 51).sum(axis=1) >= 0.001]
+    matches.reset_index(inplace=True, drop=True)
+    if len(matches) <= 0:
+        matches = pd.DataFrame([[1,3],[2,4]])
+        proof = pd.DataFrame([[0,0,0,0]])
+        proof.columns = ["MZ","FRAGS","PPM","INT"]
+        return(proof)    
+    matches_ions = pd.DataFrame()
+    for mi in list(range(0,len(matches))):
+        for ci in list(range(0, len(assign))):
+            if abs(matches.iloc[mi,0]-assign.iloc[ci,0])/assign.iloc[ci,0]*1000000 <= 51:
+                asign = pd.Series([matches.iloc[mi,0], assign.iloc[ci,1], matches.iloc[mi,1]])
+                matches_ions = pd.concat([matches_ions, asign], ignore_index=True, axis=1)
+                #matches.iloc[2,1]
+    matches_ions = matches_ions.T
+    if matches_ions.empty:
+        proof = pd.DataFrame([[0,0,0,0]])
+        proof.columns = ["MZ","FRAGS","PPM","INT"]
+        return(proof, False) 
+    matches_ions.columns = ["MZ","FRAGS","PPM"]
+    proof = pd.merge(matches_ions, ions[['MZ','INT']], how="left", on="MZ")
+    if len(proof)==0:
+        mzcycle = itertools.cycle([ions.MZ.iloc[0], ions.MZ.iloc[1]])
+        proof = pd.concat([matches_ions, pd.Series([next(mzcycle) for count in range(len(matches_ions))], name="INT")], axis=1)
+    return(proof)
+
+def miniVseq(sub, plainseq, mods, pos, fr_ns, index2, mode, min_dm):
+    ## DM ##
+    parental = getTheoMH(sub.Charge, plainseq, mods, pos, True, True)
+    mim = sub.MH
+    dm = mim - parental
+    exp_spec, ions, spec_correction = expSpectrum(fr_ns, sub.FirstScan, index2, mode)
+    theo_spec = theoSpectrum(plainseq, mods, pos, len(ions), 0)
+    terrors, terrors2, terrors3, texp = errorMatrix(ions.MZ, theo_spec)
+    ## DM OPERATIONS ##
+    dm_theo_spec = theoSpectrum(plainseq, mods, pos, len(ions), dm)
+    dmterrors, dmterrors2, dmterrors3, dmtexp = errorMatrix(ions.MZ, dm_theo_spec)
+    dmterrorsmin = pd.DataFrame(np.array([dmterrors, dmterrors2, dmterrors3]).min(0)) # Parallel minima
+    dmfppm = dmterrorsmin[(dmterrorsmin < 300).sum(axis=1) >= 0.01*len(dmterrorsmin.columns)]
+    dmfppm_fake = pd.DataFrame(50, index=list(range(0,len(plainseq)*2)), columns=list(range(0,len(plainseq)*2)))
+    if dmfppm.empty: dmfppm = dmfppm_fake 
+    dmfppm = dmfppm.where(dmfppm < 50, 50) # No values greater than 50
+    ## FRAGMENT NAMES ##
+    frags = makeFrags(len(plainseq))
+    dmterrors.columns = frags.by
+    dmterrors2.columns = frags.by2
+    dmterrors3.columns = frags.by3
+    ## ASSIGN IONS WITHIN SPECTRA ##
+    assign = assignIons(theo_spec, dm_theo_spec, frags, dm, min_dm)
+    ## PPM ERRORS ##
+    if sub.Charge == 2:
+        ppmfinal = pd.DataFrame(np.array([terrors, terrors2]).min(0))
+        if dm != 0: ppmfinal = pd.DataFrame(np.array([terrors, terrors2, dmterrors, dmterrors2]).min(0))
+    elif sub.Charge < 2:
+        ppmfinal = pd.DataFrame(np.array([terrors]).min(0))
+        if dm != 0: ppmfinal = pd.DataFrame(np.array([terrors, dmterrors]).min(0))
+    elif sub.Charge >= 3:
+        ppmfinal = pd.DataFrame(np.array([terrors, terrors2, terrors3]).min(0))
+        if dm != 0: ppmfinal = pd.DataFrame(np.array([terrors, terrors2, terrors3, dmterrors, dmterrors2, dmterrors3]).min(0))
+    else:
+        sys.exit('ERROR: Invalid charge value!')
+    ppmfinal["minv"] = ppmfinal.apply(lambda x: x.min() , axis = 1)
+    minv = ppmfinal["minv"]
+    ppmfinal = ppmfinal.drop("minv", axis=1)
+    fppm = ppmfinal[(ppmfinal < 50).sum(axis=1) >= 0.001] 
+    fppm = fppm.T
+    if fppm.empty:
+        fppm = pd.DataFrame(50, index=list(range(0,len(plainseq)*2)), columns=list(range(0,len(plainseq)*2)))
+        fppm = fppm.T
+    ## ABLINES ##
+    proof = makeAblines(texp, minv, assign, ions)
+    proof.INT = proof.INT * spec_correction
+    proof.INT[proof.INT > max(exp_spec.REL_INT)] = max(exp_spec.REL_INT) - 3
+    return(ions, proof)
+
+def parallelFragging(query, parlist):
     m_proton = 1.007276
     scan = query.scannum
     charge = query.charge
     MH = query.precursor_neutral_mass + (m_proton*charge)
     plain_peptide = query.peptide
     sequence = insertMods(plain_peptide, query.modification_info)
-    retention_time = query.retention_time
-    
-    return
+    # Make a Vseq-style query
+    sub = pd.Series([scan, charge, MH, sequence],
+                    index = ["FirstScan", "Charge", "MH", "Sequence"])
+    ions, proof = miniVseq(sub, plain_peptide, parlist[0], parlist[1], parlist[2], parlist[3])
+    hscore = hyperscore(ions, proof)
+    return(hscore)
 
 def main(args):
     '''
@@ -133,6 +398,7 @@ def main(args):
     '''
     # Parameters
     chunks = float(mass._sections['Parameters']['batch_size'])
+    min_dm = float(mass._sections['Parameters']['min_dm'])
     # Read results file from MSFragger
     df = pd.read_csv(Path(args.infile), sep="\t")
     # Read raw file
@@ -143,18 +409,13 @@ def main(args):
     tqdm.pandas(position=0, leave=True)
     if len(df) <= chunks:
         chunks = len(df)/args.n_workers
+    parlist = [msdata, index2, mode, min_dm]
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.n_workers) as executor:
         refrags = list(tqdm(executor.map(parallelFragging,
                                          rowSeries,
+                                         itertools.repeat(parlist),
                                          chunksize=chunks),
                             total=len(rowSeries)))
-        
-    
-    
-    # Make a Vseq-style query
-    # sub = pd.Series([],
-    #                 index = ["FirstScan", "Charge", "MH", "Sequence",
-    #                          "RetentionTime", "msdataDir", "outDir"])
         
         
     return
