a9eaf8bdc2da0a682fd08d264f70c013e1fa8231
Andrea <andrea.laguillo@externo.cnic.es>
2022-06-29 14:02:20 +0200

    faster makeAblines
    Before: 5.04 seconds
    After: 0.09 seconds

--- ReFrag.py
+++ ReFrag.py
@@ -279,18 +279,26 @@ def makeAblines(texp, minv, assign, ions):
         proof.columns = ["MZ","FRAGS","PPM","INT"]
         return(proof)    
     matches_ions = pd.DataFrame()
-    for mi in list(range(0,len(matches))):
-        for ci in list(range(0, len(assign))):
-            if abs(matches.iloc[mi,0]-assign.iloc[ci,0])/assign.iloc[ci,0]*1000000 <= 51:
-                asign = pd.Series([matches.iloc[mi,0], assign.iloc[ci,1], matches.iloc[mi,1]])
-                matches_ions = pd.concat([matches_ions, asign], ignore_index=True, axis=1)
-                #matches.iloc[2,1]
-    matches_ions = matches_ions.T
+    matches_ions = pd.DataFrame(list(itertools.product(list(range(0, len(matches))), list(range(0, len(assign))))))
+    matches_ions.columns = ["mi", "ci"]
+    matches_ions["temp_ci"] = list(assign.iloc[matches_ions.ci,0])
+    matches_ions["temp_mi"] = list(matches.iloc[matches_ions.mi,0])
+    matches_ions["temp_ci1"] = list(assign.iloc[matches_ions.ci,1])
+    matches_ions["temp_mi1"] = list(matches.iloc[matches_ions.mi,1])
+    matches_ions["check"] = abs(matches_ions.temp_mi-matches_ions.temp_ci)/matches_ions.temp_ci*1000000
+    matches_ions = matches_ions[matches_ions.check<=51]
+    matches_ions = matches_ions.drop(["mi", "ci", "temp_ci", "check"], axis = 1)
+    matches_ions.columns = ["MZ","FRAGS","PPM"]
+    # for mi in list(range(0, len(matches))): # TODO: has to be faster for ReFrag reruns
+    #     for ci in list(range(0, len(assign))):
+    #         if abs(matches.iloc[mi,0]-assign.iloc[ci,0])/assign.iloc[ci,0]*1000000 <= 51:
+    #             asign = pd.Series([matches.iloc[mi,0], assign.iloc[ci,1], matches.iloc[mi,1]])
+    #             matches_ions = pd.concat([matches_ions, asign], ignore_index=True, axis=1)
+    # matches_ions = matches_ions.T
     if matches_ions.empty:
         proof = pd.DataFrame([[0,0,0,0]])
         proof.columns = ["MZ","FRAGS","PPM","INT"]
-        return(proof, False) 
-    matches_ions.columns = ["MZ","FRAGS","PPM"]
+        return(proof, False)
     proof = pd.merge(matches_ions, ions[['MZ','INT']], how="left", on="MZ")
     if len(proof)==0:
         mzcycle = itertools.cycle([ions.MZ.iloc[0], ions.MZ.iloc[1]])
@@ -328,7 +336,7 @@ def miniVseq(sub, plainseq, mods, pos, mass, ftol):
     ppmfinal["minv"] = ppmfinal.apply(lambda x: x.min() , axis = 1)
     minv = ppmfinal["minv"]
     ## ABLINES ##
-    proof = makeAblines(texp, minv, assign, ions)
+    proof = makeAblines(texp, minv, assign, ions) # TODO: has to be faster for ReFrag reruns
     proof.INT = proof.INT * spec_correction
     proof.INT[proof.INT > max(exp_spec.REL_INT)] = max(exp_spec.REL_INT) - 3
     proof = proof[proof.PPM<=ftol]
