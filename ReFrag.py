f74846c88019b4ef3cf2cd33503045f028d45d48
Andrea <andrea.laguillo@externo.cnic.es>
2025-05-28 14:24:36 +0200

    Do not consider last ion of b series

--- ReFrag.py
+++ ReFrag.py
@@ -452,7 +452,7 @@ def makeFrags(seq): # TODO: SLOW
     #     # Disallowed y ions
     #     yh = [pos+1 for pos, char in enumerate(seq[::-1]) if char == 'H']
     seq_len = len(seq)
-    blist = list(range(1,seq_len+1))
+    blist = list(range(1,seq_len))
     blist = [i for i in blist if i not in bp + bh]
     ylist = list(range(1,seq_len+1))[::-1]
     ylist = [i for i in ylist if i not in yp + yh]
