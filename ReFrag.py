dbc0a9d45fcb91aaf28138ac10350465d4b75919
Andrea <andrea.laguillo@externo.cnic.es>
2025-10-17 16:57:11 +0200

    Skip empty spectra

--- ReFrag.py
+++ ReFrag.py
@@ -766,6 +766,11 @@ def main(args):
             #     spectra = spectra[np.where((np.array(spectra_n)>=0)&(np.array(spectra_n)<=args.scanrange[0]))[0][0]:np.where((np.array(spectra_n)>=0)&(np.array(spectra_n)<=args.scanrange[-1]))[0][-1]]
             #     spectra_n = spectra_n[np.where((np.array(spectra_n)>=0)&(np.array(spectra_n)<=args.scanrange[0]))[0][0]:np.where((np.array(spectra_n)>=0)&(np.array(spectra_n)<=args.scanrange[-1]))[0][-1]]
             peaks = [s.get_peaks() for s in spectra]
+            empty_peaks = set([i for i, p in enumerate(peaks) if len(p) == 0])
+            # Skip empty spectra
+            if len(empty_peaks) > 0:
+                peaks = [p for i, p in enumerate(peaks) if i not in empty_peaks]
+                spectra_n = [s for i, s in enumerate(spectra_n) if i not in empty_peaks]
             ions = [np.array([p[0], p[1]]) for p in peaks]
             ions0 = [np.array(p[0]) for p in peaks]
             ions1 = [np.array(p[1]) for p in peaks]
@@ -778,12 +783,6 @@ def main(args):
                 cutoff1 = [i/max(i) >= min_ratio for i in ions1]
                 ions0 = [ions0[i][cutoff1[i]] for i in range(len(ions))]
                 ions1 = [ions1[i][cutoff1[i]] for i in range(len(ions))]
-            # Return only top N peaks
-            logging.info("\t" + "Filter top N...")
-            if top_n > 0:
-                cutoff1 = [i >= i[np.argsort(i)[len(i)-top_n]] if len(i)>top_n else i>0 for i in ions1]
-                ions0 = [ions0[i][cutoff1[i]] for i in range(len(ions))]
-                ions1 = [ions1[i][cutoff1[i]] for i in range(len(ions))]
             # Remove peaks outside the min_frag_mz to max_frag_mz range
             if min_frag_mz > 0:
                 cutoff0 = ions0/max(ions0) >= min_frag_mz
@@ -793,6 +792,12 @@ def main(args):
                 cutoff0 = ions0/max(ions0) <= max_frag_mz
                 ions0 = ions0[cutoff0]
                 ions1 = ions1[cutoff0]
+            # Return only top N peaks
+            logging.info("\t" + "Filter top N...")
+            if top_n > 0:
+                cutoff1 = [i >= i[np.argsort(i)[len(i)-top_n]] if len(i)>top_n else i>0 for i in ions1]
+                ions0 = [ions0[i][cutoff1[i]] for i in range(len(ions))]
+                ions1 = [ions1[i][cutoff1[i]] for i in range(len(ions))]
             ions = [np.array([ions0[i],ions1[i]]) for i in range(len(ions))]
             # # Duplicate m/z measurement
             logging.info("\t" + "Filter duplicate m/z measurements...")
